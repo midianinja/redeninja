2bc56f8959f56405d0979acfbb157a7435a832be
José Carlos Marquetti Jr <cinzadiscos@gmail.com>
2017-04-10 17:15:53 -0300

    Pagina dos Autores
    - Adiciona pagina de template para os autores (author.php)
    - Adiciona campos extras na pagina do autor: avatar, facebook, twitter e
    video.
    - Usa a imagem do campo 'avatar' como imagem do profile.

---
+++ author.php
@@ -0,0 +1,123 @@
+<?php
+
+get_header();
+
+$show_default_title = get_post_meta( get_the_ID(), '_et_pb_show_title', true );
+
+$is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
+
+?>
+<style>
+
+.et_pb_blog_0 .et_pb_post .post-meta { font-family: 'Source Sans Pro', Helvetica, Arial, Lucida, sans-serif; }
+.et_pb_blog_0 .et_pb_post { font-family: 'Source Sans Pro', Helvetica, Arial, Lucida, sans-serif; }
+.et_pb_blog_0 .et_pb_post .entry-title { font-family: 'Source Sans Pro', Helvetica, Arial, Lucida, sans-serif !important; font-weight: bold !important; }
+.et_pb_text_1 { font-family: 'Arimo', Helvetica, Arial, Lucida, sans-serif; }
+.et_pb_divider_0:before { border-top-color: #000000; }
+.et_pb_image_0 { margin-left: 0; }
+.et_pb_text_0 { font-family: 'Droid Serif', Georgia, "Times New Roman", serif; font-weight: bold;font-size: 45px; }
+
+@media only screen and ( min-width: 981px ) {
+	.et_pb_row_0.et_pb_row { padding-top: 27px; padding-right: 0px; padding-bottom: 13px; padding-left: 0px; }
+	.et_pb_section_0.et_pb_section { padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; }
+}
+
+@media only screen and ( max-width: 980px ) {
+	.et_pb_text_0 p { line-height: 1em; }
+}
+.card_ds .post-content {
+	display: inline-block;
+	float: right;
+}
+.card_ds p.post-meta {
+	display: inline-block;
+	padding: 0;
+}
+</style>
+<div id="main-content">
+	<?php $author_ID = get_query_var( 'author' ); ?>
+
+	<article id="post-<?php the_ID(); ?>" <?php post_class( 'et_pb_post' ); ?>>
+		<div class="entry-content">
+			<div class="et_pb_section  et_pb_section_0 et_section_regular">
+
+
+
+				<div class=" et_pb_row et_pb_row_0">
+
+					<div class="et_pb_column et_pb_column_4_4  et_pb_column_0">
+
+						<div class="et_pb_text et_pb_module et_pb_bg_layout_light et_pb_text_align_left  et_pb_text_0">
+
+							<p style="text-align: center;"><?php the_author_meta('display_name',$author_ID); ?></p>
+
+						</div>
+						<div class="et_pb_module et_pb_space et_pb_divider et_pb_divider_0"></div>
+					</div>
+
+				</div>
+				<div class="et_pb_row et_pb_row_1 et_pb_row_1-4_3-4">
+					<div class="et_pb_column et_pb_column_1_4  et_pb_column_1">
+
+						<div class="et_pb_module et-waypoint et_pb_image et_pb_animation_off et_pb_image_0 et_always_center_on_mobile et-animated">
+							<img src="<?php the_author_meta('avatar',$author_ID); ?>" alt="<?php the_author_meta('display_name',$author_ID); ?>">
+
+						</div><div class="et_pb_text et_pb_module et_pb_bg_layout_light et_pb_text_align_left  et_pb_text_1">
+
+						<p><?php the_author_meta('description',$author_ID); ?><br>
+							<?php if (get_the_author_meta('twitter',$author_ID)) { ?>
+							<a href="<?php the_author_meta('twitter',$author_ID); ?>" target="_blank"><img class="alignleft wp-image-193 size-full" src="<?php echo get_stylesheet_directory_uri()."/images/twitter.png"; ?>" alt="Twitter" width="35" height="35"></a>
+							<?php } if (get_the_author_meta('facebook',$author_ID)) { ?>
+							<a href="<?php the_author_meta('facebook',$author_ID); ?>" target="_blank"><img class="alignleft wp-image-194 size-full" src="<?php echo get_stylesheet_directory_uri()."/images/facebook.png"; ?>" alt="Facebook" width="34" height="34"></a>
+							<?php } ?>
+						</p>
+
+					</div> <!-- .et_pb_text -->
+				</div>
+				<!-- COMEÇA O LOOP -->
+				<div class="et_pb_column et_pb_column_3_4  et_pb_column_2">
+
+					<div class="et_pb_module">
+						<?php echo wp_oembed_get(get_the_author_meta('video',$author_ID)); ?>
+
+					</div>
+					<div class="et_pb_blog_grid_wrapper">
+						<div class="et_pb_blog_grid clearfix et_pb_module et_pb_bg_layout_light  et_pb_blog_0" data-columns="2">
+							<?php while ( have_posts() ) : the_post(); ?>
+							<?php $rpost = get_post(get_the_ID()); ?>
+							<div class="column size-1of2">
+								<article id="<?php the_ID(); ?>" class="et_pb_post clearfix post type-post status-publish format-standard has-post-thumbnail hentry">
+
+									<div class="et_pb_image_container"><a href="<?php echo get_post_permalink($rpost->ID); ?>" class="entry-featured-image-url">
+										<?php echo get_the_post_thumbnail($rpost->ID); ?></a>
+									</div> <!-- .et_pb_image_container -->
+									<h2 class="entry-title"><a href="<?php echo get_post_permalink($rpost->ID); ?>"><?php echo $rpost->post_title; ?></a></h2>
+									<div class="card_ds">
+										<p class="post-meta">  <span class="published"><?php echo get_the_date('d, F, Y',$rpost->ID); ?></span></p><div class="post-content"><p>
+										<?php //Espaço para o social share ?>
+									</div>
+									<div class="card_ex">
+										<?php echo get_the_excerpt($rpost->ID); ?></p>
+									</div>
+								</div>
+							</article>
+						</div>
+					<?php endwhile; ?>
+				</div> <!-- .et_pb_posts -->
+			</div>
+		</div>
+		<!-- TERMINA O LOOP -->
+
+
+	</div>
+</div>
+</div>
+
+</article> <!-- .et_pb_post -->
+
+
+
+
+</div>
+
+<?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -1,7 +1,47 @@
 <?php
-	add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-	function theme_enqueue_styles() {
-	    wp_enqueue_style( 'divi', get_template_directory_uri() . '/style.css' );
-      wp_enqueue_script( 'divi', plugin_dir_url( __FILE__ ) . 'js/scripts.js', array( 'jquery', 'divi-custom-script' ), '0.1.2', true );
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+	wp_enqueue_style( 'divi', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_script( 'divi', plugin_dir_url( __FILE__ ) . 'js/scripts.js', array( 'jquery', 'divi-custom-script' ), '0.1.2', true );
+}
+
+function modify_user_contact_methods( $user_contact ) {
+	$user_contact['avatar']   = __( 'Avatar Image Link'   );
+	$user_contact['facebook']   = __( 'Facebook Link'   );
+	$user_contact['twitter'] = __( 'Twitter Link' );
+	$user_contact['video'] = __( 'Video Link' );
+
+	return $user_contact;
+}
+add_filter( 'user_contactmethods', 'modify_user_contact_methods' );
+
+function custom_avatar( $avatar, $id_or_email, $size, $default, $alt ) {
+	$user = false;
+
+	if ( is_numeric( $id_or_email ) ) {
+
+		$id = (int) $id_or_email;
+		$user = get_user_by( 'id' , $id );
+
+	} elseif ( is_object( $id_or_email ) ) {
+
+		if ( ! empty( $id_or_email->user_id ) ) {
+			$id = (int) $id_or_email->user_id;
+			$user = get_user_by( 'id' , $id );
+		}
+
+	} else {
+		$user = get_user_by( 'email', $id_or_email );
 	}
+
+	if ( $user && is_object( $user ) ) {
+
+		$avatar = get_the_author_meta('avatar',$user->ID);
+		$avatar = "<img alt='{$alt}' src='{$avatar}' class='avatar avatar-{$size} photo' height='{$size}' width='{$size}' />";
+
+	}
+
+	return $avatar;
+}
+add_filter( 'get_avatar' , 'custom_avatar' , 1 , 5 );
 ?>
