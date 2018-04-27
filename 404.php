fdd76d8d45e30b7db7fe061a6021fe59af0eceea
Rafael Bantu <rafaelgomes@quijaua.com.br>
2018-04-27 15:36:32 -0300

    pagina de erro 404 customizada #28

---
+++ 404.php
@@ -0,0 +1,78 @@
+<?php
+get_header();
+?>
+
+<style>
+.et_pb_image_1 { margin-left: 0; }
+.et_pb_text_3 { font-family: 'Source Sans Pro', Helvetica, Arial, Lucida, sans-serif; }
+.et_pb_text_1 { font-family: 'Source Sans Pro', Helvetica, Arial, Lucida, sans-serif;font-size: 18px; letter-spacing: 1px; line-height: 1.9em; margin-top: 75px !important; margin-right: 40px !important; margin-left: 40px !important; }
+.et_pb_text_1 p { line-height: 1.9em; }
+.et_pb_text_0 p { line-height: 1.9em; }
+.et_pb_text_0 { font-family: 'Source Sans Pro', Helvetica, Arial, Lucida, sans-serif;font-size: 18px; letter-spacing: 1px; line-height: 1.9em; margin-top: 75px !important; margin-right: 40px !important; margin-left: 40px !important; }
+.et_pb_image_0 { margin-top: 0px !important; text-align: center; }
+.et_pb_image_0 img { width: 100%; }
+.et_pb_divider_0:before { border-top-color: #000000; }
+
+@media only screen and ( min-width: 981px ) {
+	.et_pb_row_0.et_pb_row { padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; }
+	.et_pb_section_0.et_pb_section { padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; }
+	.bottom_ss .addtoany_shortcode {
+		text-align: center;
+	}
+}
+
+@media only screen and ( max-width: 980px ) {
+	.et_pb_text_1 { margin-right: 0px !important; margin-left: 0px !important; }
+	.et_pb_text_0 { margin-right: 0px !important; margin-left: 0px !important; }
+}
+
+@media (min-width: 480px) and (max-width: 980px)
+{
+	.et_pb_divider_0
+	{
+		display: none;
+
+	}
+}
+@media (max-width: 479px)
+{
+	.et_pb_divider_0
+	{
+		display: none;
+
+	}
+}
+.addtoany_shortcode {
+	margin: 0.5em 0 1em;
+}
+.et_pb_divider_0
+{
+	margin-bottom: 0 !important;
+}
+p.post-meta {
+	padding: 0 !important;
+	margin: 0 !important;
+}
+
+.entry-content .et_pb_row {
+	padding: 0 !important;
+}
+</style>
+<div id="main-content">
+	<div class="container">
+		<div id="content-area" class="clearfix">
+					<div class="entry-content" style="text-align:center;margin-top:-50px">
+
+									<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/gif-404.gif"  alt="deu ruim :-( página não encontrada" title="deu ruim :-( página não encontrada" />
+
+	</div> <!-- #left-area -->
+</div>
+	<div id="know-more-author">Conheça outros colunistas e suas opiniões!</div>
+	<?php nj_destaque_slider(5000); ?>
+</div> <!-- #content-area -->
+
+</div> <!-- .container -->
+</div> <!-- #main-content -->
+
+
+<?php get_footer(); ?>
